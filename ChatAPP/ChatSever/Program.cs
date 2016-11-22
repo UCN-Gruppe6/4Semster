a3ecfc3b5d9c0e756c665511ffaeed424c782e4d
tinsf <tinsf@MADS-SKOLE>
2016-11-22 08:39:40 +0100

    ..

---
+++ ChatAPP/ChatSever/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatSever
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
